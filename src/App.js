77b35b8eb211893faa6e23525541d0315be7d6ae
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2020-10-05 17:09:09 +0300

    v.0.2
    добавил сохранение и редактирование

--- src/App.js
+++ src/App.js
@@ -1,42 +1,124 @@
-import React from 'react';
+import React, { useState } from "react";
+import html2canvas from "html2canvas";
+import download from "downloadjs";
+import classNames from "classnames";
+import axios from "axios";
 
-import City from './components/City'
+import City from "./components/City";
 
-import DB from './assets/db.json'
+import DB from "./assets/db.json";
 
-import 'normalize.css'; // Note this
-import './scss/index.scss';
+import "normalize.css";
+import "./scss/index.scss";
 
-import logoPng from './assets/img/logo.png'
+import logoPng from "./assets/img/logo.png";
 
-
-console.log(DB);
 function App() {
+  const getWeather = () => {
+    axios
+      .get(
+        "https://api.weather.yandex.ru/v2/forecast?lat=55.75396&lon=37.620393&extra=true",
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "X-Yandex-API-Key": "22d965d2-5ec9-418d-a7f8-1b8719700270",
+          },
+        }
+      )
+      .then((result) => console.log(result));
+  };
+
+  const date = new Date();
+
+  const formatDate = (date) => {
+    var dd = date.getDate() + 1;
+    if (dd < 10) dd = "0" + dd;
+
+    var mm = date.getMonth() + 1;
+    if (mm < 10) mm = "0" + mm;
+
+    var yy = date.getFullYear() % 100;
+    if (yy < 10) yy = "0" + yy;
+
+    return dd + "." + mm + "." + yy;
+  };
+
+  const [inputDate, setInputDate] = useState(formatDate(date));
+  const [inputEdit, setInputEdit] = useState(false);
+  const [fixScreenshot, setFixScreenshot] = useState(false);
+
+  const onScreenshot = () => {
+    console.log(window.pageYOffset);
+    setFixScreenshot(true);
+    setTimeout(() => {
+      html2canvas(document.querySelector("#capture"), {
+        scrollY: -window.pageYOffset,
+        x: 0,
+        windowWidth: document.querySelector("#capture").scrollWidth,
+      }).then((canvas) => {
+        download(canvas.toDataURL());
+      });
+      setFixScreenshot(false);
+    }, 2000);
+  };
+
   return (
-    <div className="wrapper">
-      <div className="container">
-        <h1>Погода на 21.01.20</h1>
-        <table className="table__weather">
-          <thead>
-            <tr>
-              <th>Город</th>
-              <th>Температура</th>
-              <th>Осадки</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              DB[0].city.map((city) => (
-                <City key={city.id} {...city}></City>
-              ))
-            }
-          </tbody>
-        </table>
-        <div className="copyright">
-          <p>Прогноз погоды по данным Краснодарского краевого центра
-по гидрометеорологии и мониторингу окружающей среды</p>
-          <div className="logo"><img src={logoPng} alt="" /></div>
+    <div>
+      <div className="wrapper">
+        <div className="container" id="capture">
+          <h1>
+            Погода на&nbsp;
+            <input
+              type="text"
+              disabled={!inputEdit}
+              className={classNames("date", {
+                "date--edit": inputEdit,
+              })}
+              value={inputDate}
+              onChange={(e) => setInputDate(e.target.value)}
+            />
+          </h1>
+          <table className="table__weather">
+            <thead>
+              <tr>
+                <th>Город</th>
+                <th>Температура</th>
+                <th>Осадки</th>
+              </tr>
+            </thead>
+            <tbody>
+              {DB[0].city.map((city) => (
+                <City
+                  key={city.id}
+                  {...city}
+                  isEdit={inputEdit}
+                  fixScreen={fixScreenshot}
+                ></City>
+              ))}
+            </tbody>
+          </table>
+          <div className="copyright">
+            <p>
+              Прогноз погоды по данным Краснодарского краевого центра по
+              гидрометеорологии и мониторингу окружающей среды
+            </p>
+            <div className="logo">
+              <img src={logoPng} alt="" />
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="edit__wrapper">
+        <button className="btn" onClick={() => setInputEdit(!inputEdit)}>
+          {!inputEdit ? "Редактировать" : "Закончть редактирование"}
+        </button>
+        <button className="btn" onClick={onScreenshot} disabled={inputEdit}>
+          {fixScreenshot ? "Сохранение..." : "Сохранить"}
+        </button>
+        <button className="btn" onClick={() => getWeather()}>
+          Получить данные с Яндекс.Погоды
+        </button>
       </div>
     </div>
   );
--- src/components/City/index.jsx
+++ src/components/City/index.jsx
@@ -1,13 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import snowPng from "../../assets/img/snow.png";
+import windPng from "../../assets/img/wind.png";
+import cloudyPng from "../../assets/img/cloudy.png";
+import stormPng from "../../assets/img/storm.png";
+import rainPng from "../../assets/img/rain.png";
 import sunPng from "../../assets/img/sun.png";
+import classNames from "classnames";
 
-function City({ name, rainfall, temperature }) {
+function City({ name, rainfall, temperature, isEdit, fixScreen }) {
+  const [inputTemperature, setInputTemperature] = useState(temperature);
+  const [inputRainfall, setInputRainfall] = useState(rainfall);
+  const [selectImg, setSelectImg] = useState(snowPng);
+
+  const selectRainfall = (rainfall) => {
+    let selectInput = "";
+    console.log(rainfall);
+    switch (rainfall) {
+      case "snow":
+        selectInput = snowPng;
+        break;
+      case "wind":
+        selectInput = windPng;
+        break;
+      case "cloudy":
+        selectInput = cloudyPng;
+        break;
+      case "storm":
+        selectInput = stormPng;
+        break;
+      case "rain":
+        selectInput = rainPng;
+        break;
+      case "sun":
+        selectInput = sunPng;
+        break;
+      default:
+        break;
+    }
+
+    setSelectImg(selectInput);
+  };
+
   return (
     <tr className="weather">
       <td className="weather__city">{name}</td>
-      <td className="weather__temperature">{temperature}°C</td>
+      <td className="weather__temperature">
+        <div className="input-wrap">
+          <input
+            disabled={!isEdit}
+            className={classNames({
+              "input--edit": isEdit,
+            })}
+            onChange={(e) => {
+              setInputTemperature(e.target.value);
+            }}
+            value={inputTemperature}
+            type="text"
+          />
+          <span
+            className={classNames({
+              "fix--screenshot": fixScreen,
+            })}
+          >
+            °C
+          </span>
+        </div>
+      </td>
       <td className="weather__rainfall">
-        <img src={sunPng} alt="Снег" /> {rainfall}
+        <div className="img-wrap">
+          <img src={selectImg} alt="Снег" />
+        </div>
+        <select
+          disabled={!isEdit}
+          className={classNames({
+            "select--edit": isEdit,
+          })}
+          value={inputRainfall}
+          name="rainfall"
+          id="rainfall"
+          onChange={(e) => {
+            setInputRainfall(e.target.value);
+            selectRainfall(e.target.value);
+          }}
+        >
+          <option value="snow">Снег</option>
+          <option value="wind">Ветер</option>
+          <option value="cloudy">Облачно</option>
+          <option value="storm">Гроза</option>
+          <option value="rain">Дождь</option>
+          <option value="sun">Солнце</option>
+        </select>
       </td>
     </tr>
   );
